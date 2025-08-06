e6b5fa2749f4d8611b5516861b9080fd5bd71cd5
godsonsunny10@gmail.com <godsonsunny190@gmail.com>
2025-08-06 21:33:42 +0530

    html up

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,31 +1,5 @@
-// COUNTER JS
+// ---------------------------------
+// COMMON HEADER & FOOTER JS
+// ---------------------------------
 
-var counted = 0;
-$(document).ready(function () {
-  var oTop = $("#counter").offset().top - window.innerHeight;
-  if (counted == 0 && $(window).scrollTop() > oTop) {
-    $(".count").each(function () {
-      var $this = $(this),
-        countTo = $this.attr("data-count");
-      $({
-        countNum: $this.text(),
-      }).animate(
-        {
-          countNum: countTo,
-        },
 
-        {
-          duration: 2000,
-          easing: "swing",
-          step: function () {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function () {
-            $this.text(this.countNum);
-          },
-        }
-      );
-    });
-    counted = 1;
-  }
-});
