5d9028b789a939a74149e40dbaecf8f2c6a6a728
godsonsunny10@gmail.com <godsonsunny190@gmail.com>
2025-08-16 14:26:58 +0530

    html up

--- assets/javascript/reused.js
+++ assets/javascript/reused.js
@@ -1,7 +1,11 @@
-["popup", ""].forEach((part) => {
-  fetch(`${part}.html`)
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById(part).innerHTML = data;
-    });
+// ["popup", ""].forEach((part) => {
+//   fetch(`${part}.html`)
+//     .then((response) => response.text())
+//     .then((data) => {
+//       document.getElementById(part).innerHTML = data;
+//     });
+// });
+
+$(function () {
+  $("#exampleModal").load("popup.html");
 });
